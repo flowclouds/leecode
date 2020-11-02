8e93d7b52a8277c77119ef4abfd6b557afeb5af9
liangjian <470999707@qq.com>
2020-11-02 13:38:28 +0800

    完成第349题

---
+++ src/leetcode/editor/cn/a_349.java
@@ -0,0 +1,64 @@
+//给定两个数组，编写一个函数来计算它们的交集。
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+//输出：[2]
+// 
+//
+// 示例 2： 
+//
+// 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+//输出：[9,4] 
+//
+// 
+//
+// 说明： 
+//
+// 
+// 输出结果中的每个元素一定是唯一的。 
+// 我们可以不考虑输出结果的顺序。 
+// 
+// Related Topics 排序 哈希表 双指针 二分查找 
+// 👍 261 👎 0
+
+
+package leetcode.editor.cn;
+
+import java.util.*;
+
+public class a_349 {
+    public static void main(String[] args) {
+        Solution solution = new a_349().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] intersection(int[] nums1, int[] nums2) {
+
+            Set<Object> n1Set = new HashSet<>();
+            List<Integer> integers = new ArrayList<>();
+            for (int i : nums1) {
+                n1Set.add(i);
+            }
+
+            for (int i : nums2) {
+                boolean remove = n1Set.remove(i);
+                if (remove) {
+                    integers.add(i);
+                }
+
+            }
+
+            int[] ints = new int[integers.size()];
+            for (int i = 0; i < ints.length; i++) {
+                ints[i] = integers.get(i);
+            }
+            return ints;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}

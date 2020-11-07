02e8e16060dd15350964fdf061cdfc1b40007e8e
liangjian <470999707@qq.com>
2020-11-07 21:58:28 +0800

    移除异常[141]环形链表

--- src/main/java/leetcode/editor/cn/a_141.java
+++ src/main/java/leetcode/editor/cn/a_141.java
@@ -75,15 +75,15 @@ public class a_141 {
      * }
      */
 
-//    class ListNode {
-//        int val;
-//        ListNode next;
-//
-//        ListNode(int x) {
-//            val = x;
-//            next = null;
-//        }
-//    }
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
 
     public class Solution {
         public boolean hasCycle(ListNode head) {

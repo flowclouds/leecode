66f093497ee2b9cb02a830bbbc53514c581995f8
liangjian <470999707@qq.com>
2020-11-19 16:32:25 +0800

    优化✔[167]两数之和 II - 输入有序数组  为双指针

--- src/main/java/leetcode/editor/cn/a_167.java
+++ src/main/java/leetcode/editor/cn/a_167.java
@@ -29,11 +29,21 @@ public class a_167 {
     class Solution {
         public int[] twoSum(int[] numbers, int target) {
 
-            for (int i = 0; i < numbers.length - 1; i++) {
-                for (int j = i + 1; j < numbers.length; j++) {
-                    if (numbers[i] + numbers[j] == target) {
-                        return new int[]{i + 1, j + 1};
-                    }
+            int left = 0;
+            int right = numbers.length - 1;
+            while (left < right) {
+
+                int temp = numbers[left] + numbers[right];
+
+                if (temp == target) {
+                    return new int[]{left + 1, right + 1};
+                }
+
+                if (temp > target) {
+                    --right;
+                }
+                if (temp < target) {
+                    ++left;
                 }
             }
             return new int[]{};

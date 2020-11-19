72c55c2391658f0f5e9aef0dcc63522dadf27984
liangjian <470999707@qq.com>
2020-11-19 13:38:19 +0800

    保存❓[15]三数之和 本次提交超时待优化

---
+++ src/main/java/leetcode/editor/cn/a_15.java
@@ -0,0 +1,68 @@
+//给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复
+//的三元组。 
+//
+// 注意：答案中不可以包含重复的三元组。 
+//
+// 
+//
+// 示例： 
+//
+// 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+//
+//满足要求的三元组集合为：
+//[
+//  [-1, 0, 1],
+//  [-1, -1, 2]
+//]
+// 
+// Related Topics 数组 双指针 
+// 👍 2757 👎 0
+
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class a_15 {
+    public static void main(String[] args) {
+        Solution solution = new a_15().new Solution();
+        List<List<Integer>> lists = solution.threeSum(new int[]{0, 0, 0, 0});
+        System.out.println(lists);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> threeSum(int[] nums) {
+//[-4, -1, -1, 0, 1, 2]
+            Arrays.sort(nums);
+
+            System.out.println(Arrays.toString(nums));
+
+            List<List<Integer>> res = new LinkedList<>();
+            for (int i = 0; i < nums.length - 2; i++) {
+                //和上次不同
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+                for (int j = i + 1; j < nums.length - 1; j++) {
+                    //和上次不同
+                    if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                    for (int k = j + 1; k < nums.length; k++) {
+                        if (nums[i] + nums[j] + nums[k] == 0) {
+                            List<Integer> integers = new ArrayList<>();
+                            integers.add(nums[i]);
+                            integers.add(nums[j]);
+                            integers.add(nums[k]);
+                            res.add(integers);
+                            break;
+                        }
+                    }
+                }
+            }
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}

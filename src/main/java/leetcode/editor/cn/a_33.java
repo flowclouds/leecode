e66d74b4bb2210be261780e00c31c9add47f83f2
liangjian <470999707@qq.com>
2020-11-26 15:29:30 +0800

    提交✔[33]搜索旋转排序数组

--- src/main/java/leetcode/editor/cn/a_33.java
+++ src/main/java/leetcode/editor/cn/a_33.java
@@ -46,16 +46,32 @@ package leetcode.editor.cn;
 public class a_33 {
     public static void main(String[] args) {
         Solution solution = new a_33().new Solution();
+
+        int search = solution.search(new int[]{1, 3, 1, 1, 1}, 3);
+        System.out.println(search);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int search(int[] nums, int target) {
             int length = nums.length;
-            for (int i = 0; i < length; i++) {
-                if (nums[i] == target) {
-                    return i;
-                }
+            if (length == 0) return -1;
+            if (length == 1) return nums[0] == target ? 0 : -1;
+
+            int left = 0;
+            int right = length - 1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) return mid;
+                    //前半部分有序
+                else if (nums[left] <= nums[mid])
+                    if (target >= nums[left] && target < nums[mid]) right = mid - 1;
+                    else left = mid + 1;
+                    //后半部分有序
+                else if (nums[left] > nums[mid])
+                    if (target > nums[mid] && target <= nums[right]) left = mid + 1;
+                    else right = mid - 1;
             }
             return -1;
         }

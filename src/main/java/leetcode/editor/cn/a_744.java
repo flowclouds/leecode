da2f912767c9cb772d3a6c65661ab739093c4a3e
liangjian <470999707@qq.com>
2020-11-27 16:30:41 +0800

    提交✔[744]寻找比目标字母大的最小字母

---
+++ src/main/java/leetcode/editor/cn/a_744.java
@@ -0,0 +1,80 @@
+//给你一个排序后的字符列表 letters ，列表中只包含小写英文字母。另给出一个目标字母 target，请你寻找在这一有序列表里比目标字母大的最小字母。
+//
+// 在比较时，字母是依序循环出现的。举个例子： 
+//
+// 
+// 如果目标字母 target = 'z' 并且字符列表为 letters = ['a', 'b']，则答案返回 'a' 
+// 
+//
+// 
+//
+// 示例： 
+//
+// 输入:
+//letters = ["c", "f", "j"]
+//target = "a"
+//输出: "c"
+//
+//输入:
+//letters = ["c", "f", "j"]
+//target = "c"
+//输出: "f"
+//
+//输入:
+//letters = ["c", "f", "j"]
+//target = "d"
+//输出: "f"
+//
+//输入:
+//letters = ["c", "f", "j"]
+//target = "g"
+//输出: "j"
+//
+//输入:
+//letters = ["c", "f", "j"]
+//target = "j"
+//输出: "c"
+//
+//输入:
+//letters = ["c", "f", "j"]
+//target = "k"
+//输出: "c"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// letters长度范围在[2, 10000]区间内。 
+// letters 仅由小写字母组成，最少包含两个不同的字母。 
+// 目标字母target 是一个小写字母。 
+// 
+// Related Topics 二分查找 
+// 👍 103 👎 0
+
+
+package leetcode.editor.cn;
+
+public class a_744 {
+    public static void main(String[] args) {
+        Solution solution = new a_744().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public char nextGreatestLetter(char[] letters, char target) {
+            int left = 0;
+            int right = letters.length;
+
+            while (left < right) {
+                int mid = (left + right) >>> 1;
+                if (letters[mid] <= target) left = mid + 1;
+                else right = mid;
+            }
+            return letters[left % letters.length];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
